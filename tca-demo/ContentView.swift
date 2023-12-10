ec310581942613a857299d26520d65b5783136f7
Dennis van Mazijk <40456289+dennisvm82@users.noreply.github.com>
2023-12-10 21:03:44 +0900

    Initial Commit

---
+++ tca-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tca-demo
+//
+//  Created by Dennis van Mazijk on 2023/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tca-demo/tca_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_demoApp.swift
+//  tca-demo
+//
+//  Created by Dennis van Mazijk on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct tca_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
