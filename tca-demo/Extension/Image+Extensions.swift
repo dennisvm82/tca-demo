fb1d8dc529ee1b7ad8cf41b46ff3552b4e8a5e00
Dennis van Mazijk <40456289+dennisvm82@users.noreply.github.com>
2024-05-03 17:10:02 +0900

    Added Image+Extensions

---
+++ tca-demo/Extension/Image+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Image+Extensions.swift
+//  tca-demo
+//
+//  Created by Dennis van Mazijk on 2024/05/03.
+//
+
+import SwiftUI
+
+extension Image {
+    enum SFSymbols {
+        static var checkmark: Image { Image(systemName: "checkmark") }
+    }
+}
--- tca-demo/Main/DayScheduleDetail/DayScheduleDetailView.swift
+++ tca-demo/Main/DayScheduleDetail/DayScheduleDetailView.swift
@@ -39,7 +39,7 @@ struct DayScheduleDetailRow: View {
             Spacer()
             
             if isSelected {
-                Image(systemName: "checkmark")
+                Image.SFSymbols.checkmark
                     .foregroundStyle(.blue)
             }
         }
